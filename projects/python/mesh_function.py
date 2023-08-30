4db6eb30b5a7b74c1715ff5793a63f8c8cf02480
Karen Samseth <karensam@math.uio.no>
2023-08-30 13:52:45 +0200

    Omskriver mesh_function. Printer.

--- projects/python/mesh_function.py
+++ projects/python/mesh_function.py
@@ -2,23 +2,27 @@ import numpy as np
 
 
 def mesh_function(f, t):
-    t0 = t(0)
-    u0 = f(0)
-    I = u0
-    T = t(-1)
-    Nt = len(t)
-    dt = T/Nt
+    dt = 0.1
+    Nt = int(t(-1)/dt)
+    T = Nt*dt
     u = np.zeros(Nt+1)
-    u = f(t)
+    t_i = t(0)
+    for i in range(Nt+1):
+        u[i] = f(t_i)
+        t_i = t_i + dt
     return u
 
 def func(t):
+    f_mesh = np.zeros(len(t))
     for i in range(len(t)+1):
         if t[i]<=0 & t[i]>=3:
             f_mesh[i] = e**(-t[i])
         elif t[i]<=3 & t[i]>=4:
             f_mesh[i] = e**(-3*t[i])
     return f_mesh
+
+t = linspace(0,4)
+print(mesh_function(func(t),t))
 
 def test_mesh_function():
     t = np.array([1, 2, 3, 4])
