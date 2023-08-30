8e7de14c25c0b06d3cf2e4085b3b83f6fcd018dc
Karen Samseth <karensam@math.uio.no>
2023-08-30 14:11:52 +0200

    Legger inn doc string

--- projects/python/mesh_function.py
+++ projects/python/mesh_function.py
@@ -2,6 +2,14 @@ import numpy as np
 
 
 def mesh_function(f, t):
+    """
+    Input: 
+    f Python function
+    t array of mesh points
+    Nt number of mesh points in t
+    Return:
+    an array with mesh point values for f.
+    """
     dt = 0.1
     Nt = int(t(-1)/dt)
     T = Nt*dt
@@ -13,13 +21,21 @@ def mesh_function(f, t):
     return u
 
 def func(t):
-    f_mesh = np.zeros(len(t))
+    """
+    Input:
+    t array of mesh points
+    Returns:
+    f an array with points, calculated by
+        f(t) = e**(-t) for 0<=t<=3
+        f(t) = e**(-3t) for 3<t<=4
+    """
+    f = np.zeros(len(t))
     for i in range(len(t)+1):
         if t[i]<=0 & t[i]>=3:
-            f_mesh[i] = e**(-t[i])
-        elif t[i]<=3 & t[i]>=4:
-            f_mesh[i] = e**(-3*t[i])
-    return f_mesh
+            f[i] = e**(-t[i])
+        elif t[i]<3 & t[i]>=4:
+            f[i] = e**(-3*t[i])
+    return f
 
 t = linspace(0,4)
 print(mesh_function(func(t),t))
