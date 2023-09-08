9048310ca5042727b02428e7be78934295100173
Karen Samseth <karensam@math.uio.no>
2023-09-08 14:17:51 +0200

    Endrer til matriseløsningsmetode

--- projects/python/VibFD.py
+++ projects/python/VibFD.py
@@ -12,6 +12,7 @@ FUNKER IKKE ENDA!
 import numpy as np
 import matplotlib.pyplot as plt
 import sympy as sp
+from scipy import sparse
 
 t = sp.Symbol('t')
 
@@ -143,11 +144,16 @@ class VibFD2(VibSolver):
         assert T.is_integer() and T % 2 == 0
 
     def __call__(self):
-        u = np.zeros(self.Nt+1)
-        u[0] = self.I #spesifikk grense, u(0)=I
-        for n in range(1, self.Nt): #samme som i VibHPL
-            u[n+1] = 2*u[n] - u[n-1] - self.dt**2*self.w**2*u[n]
-        u[-1] = self.I #spesifikk grense, u(T)=I
+        D2 = sparse.diags([np.ones(self.Nt), np.full(self.Nt+1, -2), np.ones(self.Nt)], np.array([-1, 0, 1]), (self.Nt+1, self.Nt+1), 'lil')
+        D2 *= (1/self.dt**2) 
+        Id = sparse.eye(self.Nt+1)
+        A = (D2 + self.w**2*Id).tolil()
+        A[0,:3] = 1, 0, 0 #u(0)=I
+        A[self.Nt,-3:] = 0, 0, 1 #u(T)=I
+        b = np.zeros(self.Nt+1)
+        b[0] = self.I
+        b[-1] = self.I
+        u = sparse.linalg.spsolve(A.tocsr(), b)
         return u
 
 class VibFD3(VibSolver):
@@ -167,11 +173,17 @@ class VibFD3(VibSolver):
         assert T.is_integer() and T % 2 == 0
 
     def __call__(self):
-        u = np.zeros(self.Nt+1)
-        u[0] = self.I #u(0)=0
-        for n in range(1, self.Nt): #samme som i VibHPL
-            u[n+1] = 2*u[n] - u[n-1] - self.dt**2*self.w**2*u[n]
-        u[-1] = (2*u[-2])/(2-self.dt**2*self.w**2) #u'(T)=0
+        D2 = sparse.diags([np.ones(self.Nt), np.full(self.Nt+1, -2), np.ones(self.Nt)], np.array([-1, 0, 1]), (self.Nt+1, self.Nt+1), 'lil')
+        D2 *= (1/self.dt**2) 
+        Id = sparse.eye(self.Nt+1)
+        A = D2 + self.w**2*Id
+        A[0,:3] = 1, 0, 0 #u(0)=b(0)
+        A[self.Nt,-3:] = 0, 0, 1 #u(T)=b(T)
+        b = np.zeros(self.Nt+1)
+        b[0] = self.I #u(0)=I
+        b[-1] = (2*u[-2])/(2-self.dt**2*self.w**2) #u'(T)=0
+        A.toarray()
+        u = sparse.linalg.spsolve(A, b)
         return u
 
 class VibFD4(VibFD2):
@@ -184,17 +196,21 @@ class VibFD4(VibFD2):
     """
     order = 4
 
-    def __call__(self):
-        u = np.zeros(self.Nt+1)
-        u[0] = self.I #spesifikk grense, u(0)=I
-        l = 15-12*self.dt**2*self.w**2 #konstant til n=1 og n=N-1
-        u[1] = (u[5]-6*u[4]+14*u[3]-4*u[2]+10*u[0])/l #skewed scheme
-        k = -30+12*self.det**2*self.w**2 #konstant til de mellom
-        for n in range(2, self.Nt-1): 
-            u[n+2] = 16*u[n+1]+k*u[n]+15*u[n-1]-u[n-2] #4.ordens sentral d.s.
-        u[-1] = self.I #spesifikk grense, u(T)=I
-        u[-2] = (10*u[-1]-4*u[-3]+14*u[-4]-6*u[-5]+u[-6])/l #skewed scheme
-        return u
+def __call__(self):
+    D4 = sparse.diags([-np.ones(self.Nt), 16.*np.ones(self.Nt), np.full(self.Nt+1, -30), 16.*np.ones(self.Nt), -np.ones(self.Nt)], np.array([-2,-1, 0, 1,2]), (self.Nt+1, self.Nt+1), 'lil')
+    D4 *= (1/self.dt**2) 
+    Id = sparse.eye(self.Nt+1)
+    A = D4 + self.w**2*Id
+    A[0,:6] = 1, 0, 0, 0, 0, 0 #u(0)=b(0)
+    A[1,:6] = 10, -15, -4, 14, -6, 1 #skewed scheme for n=1
+    A[self.Nt-1,-6:] = 1, -6, 14, -4, -15, 10 #skewed scheme for n=N-1
+    A[self.Nt,-3:] = 0, 0, 1 #u(T)=b(T)
+    b = np.zeros(self.Nt+1)
+    b[0] = self.I #u(0)=I
+    b[-1] = self.I #u(T)=I
+    A.toarray()
+    u = sparse.linalg.spsolve(A, b)
+    return u
 
 def test_order(): #fra Michael
     w = 0.35
@@ -204,4 +220,7 @@ def test_order(): #fra Michael
     VibFD4(8, 2*np.pi/w, w).test_order(N0=20)
 
 if __name__ == '__main__':
-    test_order()
+    #test_order()
+    w = 0.35
+    v = VibFD3(32, 2*np.pi/w, w)
+    print(v.l2_error())
